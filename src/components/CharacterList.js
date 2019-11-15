787fb5cbdf77c66d95d094a211a078b4ac9c81db
lowlyHero <e.nunn1009@gmail.com>
2019-11-15 12:44:44 -0500

    axios call added to get chars. Router imported in required components

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import CharacterCard from './CharacterCard';
+
+import Axios from 'axios';
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [chars, setChars] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    const getChars = () => {
+    Axios
+    .get(`https://rickandmortyapi.com/api/character/`)
+    .then(res => {
+      setChars(res.data)
+    })
+    .catch(e => {
+      console.log(e)
+    });
+  }
+    
+    getChars()//  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {chars.map(char => {
+
+      })}
     </section>
   );
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Route, Link } from 'react-router-dom';
 
 export default function WelcomePage() {
   return (

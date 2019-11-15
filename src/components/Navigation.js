6db87577ceb995c0633bd7be353207850fcf30f6
lowlyHero <e.nunn1009@gmail.com>
2019-11-15 18:17:09 -0500

    Search functionality works, not exactly the way I want it to. But it does.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SearchForm from './SearchForm';
 
 const Navigation = () => {
     return ( 
         <nav>
             <Link to='/'>Home</Link>
             <Link to='/characters'>Characters</Link>
+            <SearchForm />
         </nav>
      );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
-export default function SearchForm() {
+const SearchForm = () => {
+  const [results, setResults] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios
+    .get(`https://rickandmortyapi.com/api/character/`)
+    .then(res => {
+      const characters = res.data.results.filter(character => 
+        character.name.toLowerCase().includes(search.toLowerCase())
+      );
+    setResults(characters)
+    })
+  }, [search])
+
+  const handleChange = e => {
+    setSearch(e.target.value);
+  };
  
   return (
     <section className="search-form">
-     // Add a search form here
+     <form>
+       <input
+         type='text'
+         placeholder='Search Characters'
+         value={search}
+         onChange={handleChange}
+         name='character'
+       />
+       <button type='submit'>Search</button>
+     </form>
+     <div>
+       {results.map(char => {
+         return (
+        <div>
+         <img src={char.image} />
+         <h2>{char.name}</h2>
+        </div>
+         )
+       })}
+     </div>
     </section>
   );
 }
+
+export default SearchForm;
